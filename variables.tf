2322d4293048dc10cfdab5c9f33b9bf8b6cd7279
Navneet Bairwal <50173691+navneet-bairwal@users.noreply.github.com>
2025-03-05 20:06:45 +0530

    Add Terraform workspace for test-workspace

--- variables.tf
+++ variables.tf
@@ -1,52 +1,28 @@
-variable "aws_region" {
+variable "environment" {
+  description = "Environment (dev/staging/prod)"
+  type        = string
+  default     = "dev"
+
+  validation {
+    condition     = contains(["dev", "staging", "prod"], var.environment)
+    error_message = "Environment must be one of: dev, staging, prod"
+  }
+}
+
+variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
+  default     = "us-west-2"
 }
 
-variable "project_tag" {
-  description = "Value of the 'Project' tag for all resources"
+variable "project_name" {
+  description = "Project name"
   type        = string
-  default     = "learn-terraform-run-triggers"
+  default     = "test-workspace"
 }
 
-variable "vpc_cidr_block" {
-  description = "CIDR block for VPC"
+variable "owner" {
+  description = "Owner of the resources"
   type        = string
-  default     = "10.0.0.0/16"
-}
-
-variable "public_subnet_count" {
-  description = "Number of public subnets"
-  type        = number
-  default     = 2
-}
-
-variable "private_subnet_count" {
-  description = "Number of private subnets"
-  type        = number
-  default     = 2
-}
-
-variable "subnet_cidr_blocks" {
-  description = "Available cidr blocks"
-  type        = list(string)
-  default = [
-    "10.0.1.0/24",
-    "10.0.2.0/24",
-    "10.0.3.0/24",
-    "10.0.4.0/24",
-    "10.0.5.0/24",
-    "10.0.6.0/24",
-    "10.0.7.0/24",
-    "10.0.8.0/24",
-    "10.0.9.0/24",
-    "10.0.10.0/24",
-    "10.0.11.0/24",
-    "10.0.12.0/24",
-    "10.0.13.0/24",
-    "10.0.14.0/24",
-    "10.0.15.0/24",
-    "10.0.16.0/24"
-  ]
-}
+  default     = "platform-team"
+} 
